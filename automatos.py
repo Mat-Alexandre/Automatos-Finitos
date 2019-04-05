959a17719e327b9c1a95b522932982dd7ebd59a3
Matheus Alexandre <matheusasj@live.com>
2019-04-05 17:40:34 -0300

    Add files via upload

---
+++ automatos.py
@@ -0,0 +1,10 @@
+"""
+Criando os estados do AFD/AFN
+"""
+q_estados = int(input())
+estados = {}
+for i in range(0, q_estados):
+        str_state = "q"+str(i)
+        estados.setdefault(str_state, )
+
+
